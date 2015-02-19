bcca9258c76842bf4ec057e81c9c0f9e45751d75
André Goerres <andre.goerres+git@gmail.com>
2015-02-19 20:14:16 +0100

    Included a first version of webcam into GUI.

--- pyPhotoBooth.py
+++ pyPhotoBooth.py
@@ -5,16 +5,23 @@
 
 import sys
 
-from PyQt4 import QtGui, QtCore
+import numpy as np
+import cv2
 
+from PyQt4 import QtGui, QtCore
 from photoBoothUI import Ui_photoBooth
 
 
-class BoothUI(QtGui.QMainWindow):
+class BoothUI(QtGui.QWidget):
     def __init__(self, parent=None):
+        """Initialize QWidget"""
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_photoBooth()
         self.ui.setupUi(self)
+
+        # init the webcam
+        self.video_size = QtCore.QSize(711, 400)
+        self.setupCamera()
 
         # quit shortcut & button
         quit_action = QtGui.QAction('Quit', self)
@@ -22,6 +29,26 @@ class BoothUI(QtGui.QMainWindow):
         quit_action.triggered.connect(QtGui.qApp.closeAllWindows)
         self.addAction(quit_action)
         self.ui.pushButton_quit.clicked.connect(QtGui.qApp.closeAllWindows)
+
+
+    def setupCamera(self):
+        """ Initialize camera """
+        self.capture = cv2.VideoCapture(0)
+        self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, self.video_size.width())
+        self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, self.video_size.height())
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.displayVideoStream)
+        self.timer.start(30)
+
+    def displayVideoStream(self):
+        """ Read frame from camera and repaint QLabel widget. """
+        _, frame = self.capture.read()
+        frame = cv2.cvtColor(frame, cv2.cv.CV_BGR2RGB)
+        frame = cv2.flip(frame, 1)
+        image = QtGui.QImage(frame, frame.shape[1], frame.shape[0],
+                       frame.strides[0], QtGui.QImage.Format_RGB888)
+        self.ui.label_photo.setPixmap(QtGui.QPixmap.fromImage(image))
 
 
 if __name__ == "__main__":
